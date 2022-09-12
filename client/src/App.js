e0be2c4d23c71b8aab87ab4e8da176bb421c84ba
Julie Branyan <juliebranyan@gmail.com>
2022-09-11 20:48:22 -0700

    setup course component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+/*TO Dos -Next Step
+CourseDetail - This component provides the "Course Detail" screen by retrieving the detail
+for a course from the REST API's /api/courses/:id route and rendering the course. The component
+also renders a "Delete Course" button that when clicked should send a DELETE request to the
+REST API's /api/courses/:id route in order to delete a course. This component also renders
+an "Update Course" button for navigating to the "Update Course" screen.
+*/
+
+import React from 'react';
+import './styles/reset.css';
+import './styles/global.css';
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes } from 'react-router-dom';
+
+import Courses from './components/Courses';
+// import CourseDetail from './components/CourseDetail';
+// import CreateCourse from './components/CreateCourse';
+// import Header from './components/Header';
+// import UpdateCourse from './components/UpdateCourse';
+// import UserSignIn from './components/UserSignIn';
+// import UserSignOut from './components/UserSignOut';
+// import UserSignUp from './components/UserSignUp';
 
 function App() {
-  let [courses, setCourses] = useState(null);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/courses")
-
-    .then(res => res.json())
-    .then(responseData => setCourses(responseData))
-  }, [])
-
   return (
-    <div className="App">
-       {courses}
-      <header className="App-header">
-      </header>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" element=<Courses/> />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Courses.js
@@ -0,0 +1,41 @@
+/*Courses - This component provides the "Courses" screen by retrieving the list of courses from the
+REST API's /api/courses route and rendering a list of courses. Each course needs to link to its
+respective "Course Detail" screen. This component also renders a link to the "Create Course" screen. */
+
+import React, {useState, useEffect} from 'react';
+import {
+    NavLink} from 'react-router-dom';
+
+function Courses(){
+    let [courses, setCourseDetail] = useState([]);
+
+      useEffect(() => {
+        fetch("http://localhost:5000/api/courses")
+        .then(response => response.json())
+        .then(responseData => setCourseDetail(responseData))
+        console.log(courses);
+    }, []);
+
+
+    return(
+        <div className="wrap main--grid">
+            <React.Fragment>
+            {courses.map(course =>
+                <NavLink to={`/courses/${course.id}`} key={course.id} className="course--module course--link">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">{course.title}</h3>
+                </NavLink>
+            )}
+                <NavLink to="courses/create" className="course--module course--add--module">
+                    <span className="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                        New Course
+                    </span>
+                </NavLink>
+            </React.Fragment>
+        </div>
+    );
+}
+
+export default Courses;
