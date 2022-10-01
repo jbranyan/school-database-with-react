cbf843c830d821d32b9de4262189d14fa6fdde31
Julie Branyan <juliebranyan@gmail.com>
2022-09-30 22:50:20 -0700

    global state setup for user

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthProvider';
+import { Provider } from './Context';
+import './styles/reset.css';
+import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
+    <Provider>
       <App />
-    </AuthProvider>
-  </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
